37554d1aaa80b99904c593fb17ac34a059ba27af
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-03-22 16:58:59 +0100

    script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Define the URLs for the GeoJSON files
-var scenario1Url = 'https://raw.githubusercontent.com/Khansa-Gulshad/Green-roofs-GPS/main/filtered_buildings_scenario1.geojson';
-var scenario2Url = 'https://raw.githubusercontent.com/Khansa-Gulshad/Green-roofs-GPS/main/filtered_buildings_scenario2.geojson';
-var scenario3Url = 'https://raw.githubusercontent.com/Khansa-Gulshad/Green-roofs-GPS/main/filtered_buildings_scenario3.geojson';
+var scenario1Url = 'filtered_buildings_scenario1.geojson';
+var scenario2Url = 'filtered_buildings_scenario2.geojson';
+var scenario3Url = 'filtered_buildings_scenario3.geojson';
 
 // Define a function to get color based on GPS_roof value
 function getColor(d) {
