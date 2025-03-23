467affc7d14e2cdc70fec4ef9b7b2471c2655db1
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-03-23 12:12:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ function onEachFeature(feature, layer) {
   if (feature.properties) {
     layer.bindPopup(
       `<b>Greening Potential Score:</b> ${(+feature.properties.GPS_roof).toFixed(2)}<br>` +
-      `<b>Slope:</b> ${(+feature.properties.Slope).toFixed(2)}<br>` +
-      `<b>Height:</b> ${(+feature.properties.Height).toFixed(2)}<br>` +
-      `<b>Area:</b> ${(+feature.properties.Area1).toFixed(2)}<br>` +
+      `<b>Slope:</b> ${(+feature.properties.Slope).toFixed(2)}°<br>` +
+      `<b>Height:</b> ${(+feature.properties.Height).toFixed(2)} m<br>` +
+      `<b>Area:</b> ${(+feature.properties.Area1).toFixed(2)} m²<br>` +
       `<b>Shape Ratio:</b> ${(+feature.properties.shape_ratio).toFixed(2)}<br>` +
       (feature.properties.slope_category ? `<b>Slope Category:</b> ${feature.properties.slope_category}<br>` : '') +
       (feature.properties.slope_score ? `<b>Slope Score:</b> ${feature.properties.slope_score}<br>` : '')
