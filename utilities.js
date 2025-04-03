ae0175f435a707c9aaa8f879e56c890ccc05a222
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-04-03 16:44:22 +0200

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -3,6 +3,9 @@ function safeParse(value) {
   return value === null || value === undefined ? 0 : parseFloat(value);
 }
 
-// Make these available globally since they're used across files
-let districtsData, gridData, buildingsData;
-let currentScenario = 1; // Default scenario
+// utilities.js
+window.districtsData = null;
+window.gridData = null;
+window.buildingsData = null;
+window.currentScenario = 1;
+
