0042d274a2c62f26de8a207ed23333deecd215f1
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-04-03 10:30:27 +0200

    Update mapInitialization.js

--- mapInitialization.js
+++ mapInitialization.js
@@ -1,1 +1,20 @@
+// Initialize the map centered on Gdańsk
+const map = L.map('map').setView([54.352, 18.6466], 13);
 
+// Add Mapbox dark tile layer to the map
+const mapboxLayer = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2hhbnNhZ3VsIiwiYSI6ImNtOGhqcWdqMDAyb2kybHI1Mnl2MHhwYjgifQ.9Je73sehr801s1_IynnRgw',
+  {
+    tileSize: 512,
+    zoomOffset: -1,
+    attribution: '&copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+  }
+);
+mapboxLayer.addTo(map);
+
+// Add scale control with custom options
+L.control.scale({
+  imperial: false,  // Disable feet
+  metric: true,     // Use metric system (kilometers, meters)
+  position: 'bottomright' // Change position to bottom-right
+}).addTo(map);
