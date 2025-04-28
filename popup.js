8cdb0ff16de2bfb4359bcd5ed576f3f786c81673
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2025-04-28 20:43:10 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -32,9 +32,7 @@ function onEachBuildingFeature(feature, layer) {
       `<b>Slope:</b> ${(+feature.properties.Slope).toFixed(2)}°<br>` +
       `<b>Height:</b> ${(+feature.properties.Height).toFixed(2)} m<br>` +
       `<b>Area:</b> ${(+feature.properties.Area1).toFixed(2)} m²<br>` +
-      `<b>Shape Ratio:</b> ${(+feature.properties.shape_ratio).toFixed(2)}<br>` +
-      (feature.properties.slope_category ? `<b>Slope Category:</b> ${feature.properties.slope_category}<br>` : '') +
-      (feature.properties.slope_score ? `<b>Slope Score:</b> ${feature.properties.slope_score}<br>` : '')
+      `<b>Shape Ratio:</b> ${(+feature.properties.shape_ratio).toFixed(2)}<br>`
     );
   }
 }
